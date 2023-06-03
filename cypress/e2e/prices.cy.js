0b5d0d7dad3c8f43c4ae04d6270079213595f65a
Hamming77 <bsainzpg@gmail.com>
2023-06-03 21:47:54 +0200

    Tests Lesson02

--- cypress/e2e/prices.cy.js
+++ cypress/e2e/prices.cy.js
@@ -1,6 +1,7 @@
 // enables intelligent code completion for Cypress commands
 // https://on.cypress.io/intelligent-code-completion
 /// <reference types="cypress" />
+import 'cypress-map'
 
 it('confirms the item with the lowest price', () => {
   cy.visit('/')
@@ -24,18 +25,80 @@ it('confirms the item with the lowest price', () => {
   // once the inventory loads, grab the item prices
   // https://on.cypress.io/get
   // https://on.cypress.io/find
-  //
+  cy.get('.inventory_list')
+    .should('be.visible')
+    .find('.inventory_item_price')
+    .should('have.length.gt', 3)
   // from each price element, get its inner text
   // and log it to the DevTools console
   // https://on.cypress.io/then
   // Tip: find how using https://cypress.tips/search
-  // Tip 2: Cypress._.map is really universal
-  //
+  // Tip 2: Cypress._.map is really universal => Creates an array of values by running each element in collection
+    .then($priceElementsList => Cypress._.map($priceElementsList, 'innerText'))
+    .then(console.log)
   // each price string has "$" character in front
   // remove it using string "substr" method
-  //
+    .then((priceLiteralsList) => priceLiteralsList.map(s => s.slice(1)))
+    .then(console.log)
   // convert each price string into a Number
-  //
+    .then((priceStringsList) => priceStringsList.map(Number))
+    .then(console.log)
   // find the smallest price number using Cypress._.min
   // and confirm it is 7.99
+    .then($priceNumbersList => Cypress._.min($priceNumbersList))
+    .should('equal', 7.99)
+})
+
+it('confirms the item with the lowest price - cypress-map', () => {
+  cy.visit('/')
+  // Tip: grab the username and the password from the login page
+  // It is ok for now to hardcode it in the spec source here
+  //
+  // get the username field and type the standard user
+  // https://on.cypress.io/get
+  // https://on.cypress.io/type
+  cy.get('[data-test="username"]').type('standard_user')
+  // get the password field and type the password
+  cy.get('[data-test="password"]').type('secret_sauce')
+  // get the login button and click on it
+  // https://on.cypress.io/click
+  cy.get('[data-test="login-button"]').click()
+  // you should transition to the inventory page
+  // https://on.cypress.io/location
+  // see assertion examples at
+  // https://glebbahmutov.com/cypress-examples/commands/location.html
+  cy.location('pathname').should('equal', '/inventory.html')
+  // once the inventory loads, grab the item prices
+  // https://on.cypress.io/get
+  // https://on.cypress.io/find
+  cy.get('.inventory_list')
+    .should('be.visible')
+    .find('.inventory_item_price')
+    .should('have.length.gt', 3)
+  // from each price element, get its inner text
+  // and log it to the DevTools console
+  // https://on.cypress.io/then
+  // Tip: find how using https://cypress.tips/search
+  // Tip 2: Cypress._.map is really universal => Creates an array of values by running each element in collection
+    
+    // .then($priceElementsList => Cypress._.map($priceElementsList, 'innerText'))
+    // .then(console.log)
+    .map('innerText')
+    .print('strings %o')
+  // each price string has "$" character in front
+  // remove it using string "substr" method
+    // .then((priceLiteralsList) => priceLiteralsList.map(s => s.slice(1)))
+    // .then(console.log)
+    .mapInvoke('slice', 1)
+    .print('without $ %o')
+  // convert each price string into a Number
+    // .then((priceStringsList) => priceStringsList.map(Number))
+    // .then(console.log)
+    .map(Number)
+    .print('numbers %o')
+  // find the smallest price number using Cypress._.min
+  // and confirm it is 7.99
+    //.then($priceNumbersList => Cypress._.min($priceNumbersList))
+    .apply(Cypress._.min)
+    .should('equal', 7.99)
 })
