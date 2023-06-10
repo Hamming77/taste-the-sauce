f0b4b5af3bcbbdbb6a5edd6c32337fb17d3a7d1d
Hamming77 <bsainzpg@gmail.com>
2023-06-10 22:18:00 +0200

    lesson12

--- cypress/e2e/login-form.cy.js
+++ cypress/e2e/login-form.cy.js
@@ -6,22 +6,29 @@
 
 import { LoginPage } from './login.page'
 
+beforeEach(() => {
+  cy.visit('/')
+});
+
 describe('Login form', () => {
   // visit the login page before each test
   it('shows an error for empty username field', () => {
     // click on the login button without
     // entering any of the information
-    //
+    LoginPage.getLoginButton().click()
     // the login page should show the error
     // with text "Epic sadface: Username is required"
+    LoginPage.showsErrorMessage('Epic sadface: Username is required')
   })
 
   it('shows an error for empty password field', () => {
     // enter username "name" into the input field
     // and click the login button
     // without entering the password
-    //
+    LoginPage.getUsername().type('name')
+    LoginPage.getLoginButton().click()
     // the login page should show the error
     // with text "Epic sadface: Password is required"
+    LoginPage.showsErrorMessage('Epic sadface: Password is required')
   })
 })
--- cypress/e2e/login.page.js
+++ cypress/e2e/login.page.js
@@ -11,10 +11,20 @@ export const LoginPage = {
   getError() {
     return cy.get('[data-test=error]')
   },
+  getLoginButton(){
+    return cy.get('[data-test="login-button"]')
+  },
   noErrors() {
     cy.log('**there are no errors**')
     LoginPage.getError().should('not.exist')
     LoginPage.getUsername().should('not.have.class', 'error')
     LoginPage.getPassword().should('not.have.class', 'error')
+  },
+  showsErrorMessage(msg){
+    LoginPage.getError()
+      .should('be.visible')
+      .and('include.text', msg)
+    LoginPage.getUsername().should('have.class', 'error')
+    LoginPage.getPassword().should('have.class', 'error')      
   },
 }
