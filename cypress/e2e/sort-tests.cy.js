9c68750eeab6f51008021d1085daed6772f03099
Hamming77 <bsainzpg@gmail.com>
2023-06-11 07:07:31 +0200

    lesson13

--- cypress/e2e/sort-tests.cy.js
+++ cypress/e2e/sort-tests.cy.js
@@ -11,12 +11,24 @@ import 'cypress-map'
 chai.use(require('chai-sorted'))
 
 describe('sorting', () => {
+  let userCookie
+
   beforeEach(() => {
-    cy.log('**log in**')
-    cy.visit('/')
-    cy.get('[data-test="username"]').type('standard_user')
-    cy.get('[data-test="password"]').type('secret_sauce')
-    cy.get('[data-test="login-button"]').click()
+    if (userCookie) {
+      cy.setCookie('session-username', userCookie.value, userCookie)
+      cy.visit('/inventory.html')
+    } else {
+      cy.log('**log in**')
+      cy.visit('/')
+      cy.get('[data-test="username"]').type('standard_user')
+      cy.get('[data-test="password"]').type('secret_sauce')
+      cy.get('[data-test="login-button"]').click()
+      cy.location('pathname').should('equal', '/inventory.html')
+      cy.getCookie('session-username')
+        .should('exist')
+        .then((ck) => (userCookie = ck))
+    }
+
     cy.location('pathname').should('equal', '/inventory.html')
   })
 
