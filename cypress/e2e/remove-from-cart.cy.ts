32e45935ad38de2002c6d75115743b3a24c30196
Hamming77 <bsainzpg@gmail.com>
2023-06-18 08:30:42 +0200

    lesson30

--- cypress/e2e/remove-from-cart.cy.ts
+++ cypress/e2e/remove-from-cart.cy.ts
@@ -22,17 +22,38 @@ describe('Cart', () => {
   it('removes items from cart', { viewportHeight: 1200 }, () => {
     // using the inventory page object
     // add 'Sauce Labs Bike Light' and 'Sauce Labs Bolt T-Shirt' items
-    //
+    const items = [
+      'Sauce Labs Bike Light',
+      'Sauce Labs Bolt T-Shirt'
+    ]
+    // add each item to cart using the InventoryPage object
+    items.forEach(InventoryPage.addItemToCart)
     // the cart icon should show badge with number 2
+    InventoryPage.getCartBadge()
+        .should('have.text', items.length)
+        .scrollIntoView()
+        .wait(1000)
+        .click()
     // and once you click it, you should transition to the cart page
     cy.log('**we are on the cart page**')
+    cy.location('pathname').should('equal', '/cart.html')
     // there should 2 items in the cart
+    cy.get('.cart_list .cart_item').should('have.length', items.length)
     cy.log('**remove the Bike Light**')
+
     // find the cart item with text "Bike Light"
+    cy.contains('.cart_list .cart_item', items[0])
     // and click the Remove button
+      .contains('button', 'Remove')
+      .click()
     cy.log('**the T-shirt item still remains**')
     // there should be a single cart item
+    cy.get('.cart_list .cart_item')
+      .should('have.length', 1)
     // and it should have text "Bolt T-Shirt"
+      .contains(items[1])
     // the cart badge should show number 1
+    InventoryPage.getCartBadge()
+    .should('have.text', '1')
   })
 })
